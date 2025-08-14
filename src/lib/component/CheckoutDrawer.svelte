c2a0efdafad2743e4100e88d1562038d76a21800
naim <naimmmmab@gmail.com>
2025-08-14 15:10:04 +0800

    Update: 14/08/2025, 15.10.04

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -45,12 +45,13 @@
       <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
       <ul class="menu bg-base-200 text-base-content h-[95vh] w-[90vw] md:w-[30vw] mt-15 flex flex-col justify-center items-center">
 
+        tombol checkout
         <div class="w-80 flex justify-between items-center"><button class="py-2 px-4 font-bold bg-orange-500 rounded-full md:hidden" on:click={() => {
         const drawer = document.getElementById('my-drawer-4');
         if (drawer) drawer.checked = false;}}> <ArrowLeft /> </button>
         <h1 class="m-auto text-3xl">CheckOut</h1></div>
 
-        <div class="w-full h-[60vh] flex flex-col flex-wrap justify-start items-center ">
+        <div class="w-full h-[60vh] flex flex-col flex-wrap justify-start items-center overflow-scroll">
           {#each checkoutData as item, index}
             <div class="w-100 h-15 bg-black flex justify-between items-center rounded-full m-1 ">
               <img src={`http://${base}/${item.products.image_url}`} alt="" class="w-16 h-16 object-cover rounded-full">
