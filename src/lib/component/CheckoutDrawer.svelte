e569ebd2539eb67bc9221dc4af35fe18b71059d1
naim <naimmmmab@gmail.com>
2025-08-14 13:00:52 +0800

    Update: 14/08/2025, 13.00.52

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -43,14 +43,14 @@
 
     <div class="drawer-side">
       <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
-      <ul class="menu bg-base-200 text-base-content min-h-full w-[90vw] md:w-[20cw] mt-15 border flex flex-col justify-center items-center">
+      <ul class="menu bg-base-200 text-base-content min-h-full w-[90vw] md:w-[30vw] mt-15 border flex flex-col justify-center items-center">
 
         <div class="w-80 flex justify-between items-center"><button class="py-2 px-4 font-bold bg-orange-500 rounded-full md:hidden" on:click={() => {
         const drawer = document.getElementById('my-drawer-4');
         if (drawer) drawer.checked = false;}}> <ArrowLeft /> </button>
         <h1 class="m-auto text-3xl">CheckOut</h1></div>
 
-        <div class="w-full h-[50vh] md:h-[70vh] flex flex-col flex-wrap justify-start items-center overflow-scroll bg-gray-800">
+        <div class="w-full h-[50vh] md:h-[70vh] flex flex-col flex-wrap justify-start items-center overflow-scroll">
           {#each checkoutData as item, index}
             <div class="w-150 h-15 bg-black flex justify-between items-center rounded-full m-1 ">
               <div class="w-20 h-20 bg-red-500 rounded-full bg-cover bg-center"
