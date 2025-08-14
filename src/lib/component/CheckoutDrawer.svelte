b0a5228b2aeb88ad26abe90380b9c710787438f3
naim <naimmmmab@gmail.com>
2025-08-14 17:33:34 +0800

    Update: 14/08/2025, 17.33.34

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -52,7 +52,7 @@
         <h1 class="m-auto text-3xl">CheckOut</h1></div>
 
         <!-- list pesanan -->
-        <div class="w-[90vw] h-[60vh] flex flex-col flex-wrap justify-start items-center overflow-scroll">
+        <div class="w-full h-[60vh] flex flex-col flex-wrap justify-start items-center overflow-scroll">
           {#each checkoutData as item, index}
             <div class="w-100 h-15 bg-black flex justify-between items-center rounded-full m-1 ">
               <img src={`http://${base}/${item.products.image_url}`} alt="" class="w-16 h-16 object-cover rounded-full">
@@ -71,7 +71,7 @@
             </div>
           {/each}
         </div>
-
+        
         <div class="w-83 md:w-full bg-black rounded-2xl p-3 m-1 md:text-2xl">Total Rp: <span class="text-green-500">{total}</span></div>
         <div class="w-83 md:w-full bg-black rounded-2xl p-3 m-1 md:text-2xl">
           <label class="select">
