b0d979c6ee078c5d7d45b3477a2a4f4ff55ea5c2
naim <naimmmmab@gmail.com>
2025-08-14 15:01:09 +0800

    Update: 14/08/2025, 15.01.09

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -43,7 +43,7 @@
 
     <div class="drawer-side">
       <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
-      <ul class="menu bg-base-200 text-base-content h-full w-[90vw] md:w-[30vw] mt-15 bg-blue-500 border flex flex-col justify-center items-center over">
+      <ul class="menu bg-base-200 text-base-content h-full w-[90vw] md:w-[30vw] mt-15 bg-blue-500 border flex flex-col justify-center items-center ove">
 
         <div class="w-80 flex justify-between items-center"><button class="py-2 px-4 font-bold bg-orange-500 rounded-full md:hidden" on:click={() => {
         const drawer = document.getElementById('my-drawer-4');
