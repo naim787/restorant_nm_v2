17ccfb3fe9f074cc7ad6b9f3e20b56f600492cf1
naim <naimmmmab@gmail.com>
2025-08-16 14:21:57 +0800

    Update: 8/16/2025, 2:21:56 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -25,26 +25,6 @@
             <kbd class="kbd kbd-sm">⌘</kbd>
             <kbd class="kbd kbd-sm">K</kbd>
             </label>
-            <label class="input">
-            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                stroke-linejoin="round"
-                stroke-linecap="round"
-                stroke-width="2.5"
-                fill="none"
-                stroke="currentColor"
-                >
-                <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
-                <path d="M14 2v4a2 2 0 0 0 2 2h4"></path>
-                </g>
-            </svg>
-            <input type="text" class="grow" placeholder="index.php" />
-            </label>
-            <label class="input">
-            Path
-            <input type="text" class="grow" placeholder="src/app/" />
-            <span class="badge badge-neutral badge-xs">Optional</span>
-        </label>
     </div>
 
 </div>
