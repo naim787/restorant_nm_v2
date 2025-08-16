741ce47965012406188c8da8c1278d0cb75dd4da
naim <naimmmmab@gmail.com>
2025-08-16 14:27:21 +0800

    Update: 8/16/2025, 2:27:21 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import "../../app.css"
     import NavPanel from '$lib/component/Nav_Panel.svelte';
+   import { Search } from "@lucide/svelte";
 </script>
 
 <NavPanel />
@@ -8,7 +9,9 @@
 <div class="w-[100vw] h-[100vh] pt-15">
 
     <div class="w-full h-30 flex items-center justify-evenly">
-       <div class="w-[50%] h-20 shadow bg-gray-950 rounded-xl"></div>
+       <div class="w-[50%] h-20 shadow bg-gray-950 rounded-xl">
+
+       </div>
     </div>
 
 </div>
