bcb0c375d837d63803ae7e0e4e53c9a6545d3bdc
naim <naimmmmab@gmail.com>
2025-08-16 14:09:43 +0800

    Update: 8/16/2025, 2:09:42 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -1,4 +1,10 @@
 <script>
-    import "../app.css"
+    import "..//app.css"
+    import NavPanel from '$lib/component/Nav_Panel.svelte';
 </script>
 
+<Navpanel />
+
+<div class="w-[100vw] h-[100vh]">
+
+</div>
