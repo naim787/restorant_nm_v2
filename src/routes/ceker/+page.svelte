49cf3d59b04cc796c493fd99de041956200bc104
naim <naimmmmab@gmail.com>
2025-08-16 15:10:53 +0800

    Update: 8/16/2025, 3:10:53 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -9,8 +9,9 @@
 <div class="w-[100vw] h-[100vh] pt-15">
 
     <div class="w-full h-30 flex items-center justify-evenly mt-5">
-        <div class="w-[13vw] h-[5vw] bg-gray-950 rounded-3xl flex justify-start items-center">
-            <Clock class="p-2 bg-yellow-500/30" size={20}/>
+        <div class="w-[13vw] h-[5vw] bg-gray-950 rounded-3xl flex justify-start items-center ">
+            <Clock class="p-2 bg-yellow-500/30 rounded-xl" size={40}/>
+            <div class=""></div>
         </div>
     </div>
 
