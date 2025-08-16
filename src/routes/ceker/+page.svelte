76eaec4ae1c552258347dc2cf6c58baf010beb61
naim <naimmmmab@gmail.com>
2025-08-16 18:56:00 +0800

    Update: 8/16/2025, 6:55:59 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -80,7 +80,7 @@
                     </div>
                 </div>
                 {#each data.product_orders as d, i}
-                <div class="flex w-[50%] justify-between odd:bg-white/10 even: p-2">
+                <div class="flex w-[50%] justify-between p-2 border-b border-white/10">
                   <p class="text-lg">{d.products_name}</p>
                   <p class="text-3xl">{d.value}</p>  
                 </div>
