e4746c2d6efdb9d35565e65e5159ac8a79432bcf
naim <naimmmmab@gmail.com>
2025-08-16 14:28:44 +0800

    Update: 8/16/2025, 2:28:44 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -11,7 +11,7 @@
     <div class="w-full h-30 flex items-center justify-evenly">
        <div class="w-[50%] h-20 shadow bg-gray-950 rounded-xl flex items-center justify-around">
         <Search />
-        <input type="text" name="" id="" class="w-[30]" />
+        <input type="text" name="" id="" class="w-[30%]" />
        </div>
     </div>
 
