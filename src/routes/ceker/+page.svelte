7a0a06ad7bd7d7a9edb38ace03eaeb96b5098dcf
naim <naimmmmab@gmail.com>
2025-08-16 14:23:51 +0800

    Update: 8/16/2025, 2:23:50 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -8,23 +8,7 @@
 <div class="w-[100vw] h-[100vh] pt-15">
 
     <div class="w-full h-52 border">
-        <label class="input">
-            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                stroke-linejoin="round"
-                stroke-linecap="round"
-                stroke-width="2.5"
-                fill="none"
-                stroke="currentColor"
-                >
-                <circle cx="11" cy="11" r="8"></circle>
-                <path d="m21 21-4.3-4.3"></path>
-                </g>
-            </svg>
-            <input type="search" class="grow" placeholder="Search" />
-            <kbd class="kbd kbd-sm">⌘</kbd>
-            <kbd class="kbd kbd-sm">K</kbd>
-        </label>
+       
     </div>
 
 </div>
