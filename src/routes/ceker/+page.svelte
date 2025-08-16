ef972e086d888c82792ae1e1155002d67e6e2fcb
naim <naimmmmab@gmail.com>
2025-08-16 16:18:24 +0800

    Update: 8/16/2025, 4:18:24 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -8,7 +8,10 @@
     let orders = [
         {
         product_orders: [
-            { products_id: '5115', products_name: 'dagig wahyu', product_price: '5000', value: 5, total: 25000 },
+            { products_id: '5115', products_name: 'dagig wahyu', product_price: '5000', value: 5, total: 25000 , 
+status
+: 
+"pending"},
             { products_id: '4550', products_name: 'pop ice', product_price: '9000', value: 2, total: 18000 },
             { products_id: '7501', products_name: 'sapo tahu', product_price: '16000', value: 8, total: 128000 },
             { products_id: '6059', products_name: 'rahel', product_price: '600', value: 7, total: 4200 },
@@ -77,8 +80,8 @@
                     </span>
                 </div>
                 {#each data.product_orders as d, i}
-                <div class="flex">
-                  <p class="text-lg">{d.products_name}</p> : <p class="text-lg">{d.value}</p>  
+                <div class="flex w-[50%] justify-between">
+                  <p class="text-lg">{d.products_name}</p><p class="text-lg">{d.value}</p>  
                 </div>
                 {/each}
                 <div class="flex gap-3 mt-4">
