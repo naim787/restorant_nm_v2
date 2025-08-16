7d615b34308066bb2f11875a1c283eb22134e4e5
naim <naimmmmab@gmail.com>
2025-08-16 14:28:40 +0800

    Update: 8/16/2025, 2:28:39 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import "../../app.css"
     import NavPanel from '$lib/component/Nav_Panel.svelte';
-import { Search } from "@lucide/svelte";
+    import { Search } from "@lucide/svelte";
 </script>
 
 <NavPanel />
@@ -9,8 +9,9 @@ import { Search } from "@lucide/svelte";
 <div class="w-[100vw] h-[100vh] pt-15">
 
     <div class="w-full h-30 flex items-center justify-evenly">
-       <div class="w-[50%] h-20 shadow bg-gray-950 rounded-xl">
-
+       <div class="w-[50%] h-20 shadow bg-gray-950 rounded-xl flex items-center justify-around">
+        <Search />
+        <input type="text" name="" id="" class="w-[]" />
        </div>
     </div>
 
