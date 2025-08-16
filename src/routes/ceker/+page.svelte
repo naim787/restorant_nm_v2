6c9efe4c52621930eb141ee1fe7fe75e65c023b1
naim <naimmmmab@gmail.com>
2025-08-16 16:12:15 +0800

    Update: 8/16/2025, 4:12:14 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -76,7 +76,7 @@
                         Lorem, ipsum.
                     </span>
                 </div>
-                <p class="text-lg">{}</p>
+                <p class="text-lg">{data.}</p>
                 <div class="flex gap-3 mt-4">
                     <button class="flex-1 bg-green-600 hover:bg-green-700 rounded-xl py-2 text-sm">Sajikan</button>
                     <button class="flex-1 bg-red-600 hover:bg-red-700 rounded-xl py-2 text-sm">Batal</button>
