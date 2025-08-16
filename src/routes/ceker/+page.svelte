818e542d8bf15e0217ea23da900472629ca2c27c
naim <naimmmmab@gmail.com>
2025-08-16 15:20:24 +0800

    Update: 8/16/2025, 3:20:24 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     import "../../app.css"
     import NavPanel from '$lib/component/Nav_Panel.svelte';
-    import { Search, Clock, ClockAlert, <CircleAlert } from "@lucide/svelte";
+    import { Search, Clock, ClockAlert, CircleAlert } from "@lucide/svelte";
 </script>
 
 <NavPanel />
