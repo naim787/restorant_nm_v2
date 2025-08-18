4e32628fed8c0fcc5130fc2867b25087981b395e
naim <naimmmmab@gmail.com>
2025-08-18 15:26:38 +0800

    Update: 8/18/2025, 3:26:38 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -97,11 +97,11 @@
 
 <div class="w-[100vw] h-[100vh] pt-20 px-10 bg-gray-900 text-white overflow-y-auto">
   <!-- Aktifkan notifikasi manual -->
-  <button on:click={() => {audioEl.play().catch(()=>{}) }} class="bg-green-500 px-4 py-2 rounded mb-4 flex text-black">
+  <button on:click={togglePlay} class="bg-green-500 px-4 py-2 rounded mb-4 flex text-black">
     {#if isPlaying}
       <Volume2 class="mx-2"/>
-    {:else}
       <VolumeOff />
+    {:else}
     {/if}
     Aktifkan Notifikasi
   </button>
