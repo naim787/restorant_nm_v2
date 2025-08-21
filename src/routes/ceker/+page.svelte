883f0c8484b8c4285b082fc5a754255eee33f333
naim <naimmmmab@gmail.com>
2025-08-21 19:50:39 +0800

    Update: 8/21/2025, 7:50:39 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -137,10 +137,40 @@
         <div class="flex justify-between items-center mb-3">
           <h2 class="text-xl font-bold flex items-center gap-2">
             <!-- <UtensilsCrossed size={22}/> Meja : <span class="text-4xl">{data.table_id}</span> -->
-            <UtensilsCrossed size={22}/> Meja : <span class="text-4xl">1</span>
+            <UtensilsCrossed size={22}/> Meja : (<span class="">1</span>)
           </h2>
         </div>
         <!-- {#each data.product_orders as d} -->
+        <div class="flex w-[50%] justify-between p-2 border-b border-white/10 text-2xl">
+          <p class="text-lg">
+            <!-- {d.products_name} -->
+             Lorem ipsum dolor sit amet.
+          </p>
+          <p class="text-3xl text-green-400">
+            <!-- {d.value} -->
+             1
+          </p>  
+        </div>
+        <div class="flex w-[50%] justify-between p-2 border-b border-white/10 text-2xl">
+          <p class="text-lg">
+            <!-- {d.products_name} -->
+             Lorem ipsum dolor sit amet.
+          </p>
+          <p class="text-3xl text-green-400">
+            <!-- {d.value} -->
+             1
+          </p>  
+        </div>
+        <div class="flex w-[50%] justify-between p-2 border-b border-white/10 text-2xl">
+          <p class="text-lg">
+            <!-- {d.products_name} -->
+             Lorem ipsum dolor sit amet.
+          </p>
+          <p class="text-3xl text-green-400">
+            <!-- {d.value} -->
+             1
+          </p>  
+        </div>
         <div class="flex w-[50%] justify-between p-2 border-b border-white/10 text-2xl">
           <p class="text-lg">
             <!-- {d.products_name} -->
