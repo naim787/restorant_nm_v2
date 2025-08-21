d866cd39b42fddcae674a1a9faae926d770fab42
naim <naimmmmab@gmail.com>
2025-08-21 19:59:25 +0800

    Update: 8/21/2025, 7:59:24 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -137,7 +137,7 @@
         <div class="flex justify-between items-center mb-3">
           <h2 class="text-xl font-bold flex items-center gap-2">
             <!-- <UtensilsCrossed size={22}/> Meja : <span class="text-4xl">{data.table_id}</span> -->
-            <UtensilsCrossed size={22}/> Meja : (<span class="">1</span>) 19
+            <UtensilsCrossed size={22}/> Meja : (<span class="">1</span>) 19:
           </h2>
         </div>
         <!-- {#each data.product_orders as d} -->
