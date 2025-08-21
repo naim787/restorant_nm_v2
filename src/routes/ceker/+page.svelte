d4d0233d14bcc361f48913fac3d9f7afc76b3dac
naim <naimmmmab@gmail.com>
2025-08-21 19:41:24 +0800

    Update: 8/21/2025, 7:41:23 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -132,7 +132,7 @@
   <audio bind:this={audioEl} src="./out.mp3"></audio>
 
   <div class="grid grid-cols-3 gap-6">
-    {#each orders as data, index}
+    <!-- {#each orders as data, index} -->
       <div class="bg-gray-950 rounded-2xl p-6 shadow-md hover:scale-[1.02] transition">
         <div class="flex justify-between items-center mb-3">
           <h2 class="text-xl font-bold flex items-center gap-2">
