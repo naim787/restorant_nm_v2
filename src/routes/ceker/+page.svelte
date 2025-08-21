b4a97301839333684269b3212273dadac59e37a6
naim <naimmmmab@gmail.com>
2025-08-22 01:28:08 +0800

    Update: 8/22/2025, 1:28:07 AM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -133,7 +133,8 @@
 
   <!-- filter -->
    <div class="w-full flex justify-evenly items-center">
-    <div class="font-bold p-3 rounded-2xl bg-gray-950 border-gray-400 flex items-center"><AlarmClock class="text-yellow-500"/> : 0</div>
+    <div class="font-bold py-5 px-10 rounded-2xl bg-gray-950 border-gray-400 flex items-center"><AlarmClock class="text-yellow-500"/> : 0</div>
+    <div class="font-bold py-5 px-10 rounded-2xl bg-gray-950 border-gray-400 flex items-center"><AlarmClock class="text-yellow-500"/> : 0</div>
    </div>
 
   <div class="flex flex-col gap-2 justify-center items-start">
