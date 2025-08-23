d4ec114b2ec0c35a53401dd5fdcbdecf993c72c8
naim <naimmmmab@gmail.com>
2025-08-23 12:25:33 +0800

    Update: 8/23/2025, 12:25:32 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -154,7 +154,7 @@
     <div class="font-bold w-[90px] h-[70px] p-2 rounded-md xl:rounded-2xl bg-black border-gray-400 flex items-center text-1xl"><X class="text-red-500"/> : 0</div>
    </div>
 
-  <div class="flex flex-wrap gap-2 justify-start items-start mt-22 md:">
+  <div class="flex flex-wrap gap-2 justify-start items-start mt-22 md:mt-30">
     {#each orders as data, index}
      <!-- {#each orders.filter(o => o.status === "pending") as data, index} -->
 
@@ -177,7 +177,7 @@
         <div class="flex gap-3 mt-4">
 
           {#if data.status !== "diantar"}
-            <button on:click={() => finishOrder(index)} class="flex-1 bg-green-600 hover:bg-green-700 rounded-xl py-2 text-sm">Selesai</button>
+            <button on:click={() => finishOrder(index)} class="flex-2 bg-green-600 hover:bg-green-700 rounded-xl py-2 text-sm">Selesai</button>
             <button on:click={() => finishOrder(index)} class="flex-1 bg-red-600 hover:bg-red-700 rounded-xl py-2 text-sm">Cencel</button>
           {/if}
 
