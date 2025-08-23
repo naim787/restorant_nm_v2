b7074102324ede8dc9c93fae0b629b7e47dc51ed
naim <naimmmmab@gmail.com>
2025-08-23 10:37:18 +0800

    Update: 8/23/2025, 10:37:17 AM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -139,9 +139,9 @@
     <div class="font-bold w-[90px] h-[70px] p-2 rounded-md xl:rounded-2xl bg-black border-gray-400 flex items-center text-1xl"><X class="text-red-500"/> : 0</div>
    </div>
 
-  <div class="flex flex-col gap-2 justify-center items-start mt-22">
+  <div class="flex flex-col gap-2 justify-start items-center mt-22">
     {#each orders as data, index}
-      <div class="bg-gray-950 rounded-2xl p-2 shadow-md hover:scale-[1.02] transition w-auto h-auto">
+      <div class="bg- rounded-2xl p-2 shadow-md hover:scale-[1.02] transition w-auto h-auto">
         <div class="flex justify-between items-center mb-3">
           <h2 class="text-xl font-bold flex items-center gap-2">
             <UtensilsCrossed size={22}/> Meja : (<span class="">{data.table_id}</span>) <span class="font-thin text-lg">12:30:00</span>
