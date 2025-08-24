36b38e07babf436dd623266db4fa5bf94d973259
naim <naimmmmab@gmail.com>
2025-08-24 13:44:32 +0800

    Update: 8/24/2025, 1:44:32 PM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -54,7 +54,7 @@
         <!-- list pesanan -->
         <div class="w-full md:w-[30vw] border h-[50vh] flex flex-col justify-start items-center overflow-scroll">
           {#each checkoutData.slice().reverse() as item, index}
-            <div class="w-[30vw] h-[51vw] border border-red-500 bg-black flex justify-between items-center rounded-full m-1 ">
+            <div class="w-[30vw] h-[1vw] border border-red-500 bg-black flex justify-between items-center rounded-full m-1 ">
               <img src={`http://${base}/${item.products.image_url}`} alt="" class="w-16 h-16 object-cover rounded-full">
                 <div class="w-full px-2">
                   <h1>{item.products.name}</h1>
