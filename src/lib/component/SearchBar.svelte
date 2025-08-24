a37e44c24cd2e6535c2464803fda02723ed4e96f
naim <naimmmmab@gmail.com>
2025-08-24 18:37:16 +0800

    Update: 8/24/2025, 6:37:16 PM

--- src/lib/component/SearchBar.svelte
+++ src/lib/component/SearchBar.svelte
@@ -16,6 +16,6 @@
       bind:value={searchTerm}
       on:input={onInput}
     />
-    <Search class="p-2 rounded-md bg-yellow-500 text-black" size={}/>
+    <Search class="p-2 rounded-md bg-yellow-500 text-black" size={35}/>
   </div>
 {/if}
