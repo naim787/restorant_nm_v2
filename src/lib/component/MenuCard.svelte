ff3cb220276b75b37edc8ce4992d577b834ec313
naim <naimmmmab@gmail.com>
2025-08-24 19:16:30 +0800

    Update: 8/24/2025, 7:16:29 PM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -30,7 +30,7 @@
     <div class="rating">
       <div class="mask mask-star" aria-label="1 star"></div>
       <div class="mask mask-star" aria-label="2 star"></div>
-      <div class="mask mask-star" aria-label="3 star" aria-current="true"></div>
+      <div class="mask mask-star" aria-label="3 star" ></div>
       <div class="mask mask-star" aria-label="4 star"></div>
       <div class="mask mask-star" aria-label="5 star"></div>
     </div>
