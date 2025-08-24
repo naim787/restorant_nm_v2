f008fcd33271dd7769ca96b5a32646eb4bf3846c
naim <naimmmmab@gmail.com>
2025-08-24 12:28:14 +0800

    Update: 8/24/2025, 12:28:11 PM

--- src/routes/ceker/+page.svelte
+++ src/routes/ceker/+page.svelte
@@ -186,7 +186,7 @@
 
           {#if data.status !== "diantar"}
             <button on:click={() => actionOrder(index, "diantar")} class="flex-2 bg-green-600 hover:bg-green-700 rounded-xl py-2 text-sm">Selesai</button>
-            <button on:click={() => actionOrder(index, "ca")} class="flex-1 bg-red-600 hover:bg-red-700 rounded-xl py-2 text-sm">Cencel</button>
+            <button on:click={() => actionOrder(index, "cancel")} class="flex-1 bg-red-600 hover:bg-red-700 rounded-xl py-2 text-sm">Cencel</button>
           {/if}
 
         </div>
