4becb5105f79c4c857420bb125c1cd202b7f0517
naim <naimmmmab@gmail.com>
2025-08-24 22:34:30 +0800

    Update: 8/24/2025, 10:34:30 PM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -68,7 +68,7 @@
                 <div class="w-full">
                   <h1 class="text-green-500 font-bold">{item.value}</h1>
                 </div>
-              <button type="submit" class="btn bg-transparent border-none" on:click={() => removeCheckout(index)}>
+              <button type="submit" class="btn bg-transparent border-none outline-" on:click={() => removeCheckout(index)}>
                 <CircleX size={30} class="text-red-500" />
               </button>
             </div>
