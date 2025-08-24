895892e0c295e7f855b545f8b6768b3f851e32bf
naim <naimmmmab@gmail.com>
2025-08-24 20:07:53 +0800

    Update: 8/24/2025, 8:07:53 PM

--- src/lib/component/MenuModal.svelte
+++ src/lib/component/MenuModal.svelte
@@ -108,7 +108,7 @@
       </div>
 
         <div class="flex justify-between items-center ">
-          <div class="p-2 bg-gray-900 rounded-full flex justify-center items-center border border-amber-300 w-[">
+          <div class="p-2 bg-gray-900 rounded-full flex justify-center items-center border border-amber-300 w-[]">
                   <button type="button" class="py-1 px-3 bg-red-500 rounded-full"
                     on:click={() => onValueChange(Math.max(1, Value - 1))}>-</button>
         
