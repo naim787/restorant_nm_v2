48508053b88d8b525dff95d3c3be27ced42aa54d
naim <naimmmmab@gmail.com>
2025-08-24 20:58:26 +0800

    Update: 8/24/2025, 8:58:25 PM

--- src/lib/component/MenuModal.svelte
+++ src/lib/component/MenuModal.svelte
@@ -120,7 +120,7 @@
                   <button type="button" class="py-1 px-3 bg-green-500 rounded-full"
                     on:click={() => onValueChange(Value + 1)}>+</button>
           </div>
-            <button class="text-[5vw] font-bold sm:text-xs py-2 px-4 bg-blue-500 rounded-md text-black" on:click={onCheckout}><ShoppingCart /></button>
+            <button class="text-[5vw] font-bold sm:text-xs py-2 px-4 bg-blue-500 rounded-md text-black" on:click={onCheckout}></button>
         </div>
     </article>
   </div>
