25a6f4bf2d78b2624b53c20068cb35c8f3077802
naim <naimmmmab@gmail.com>
2025-08-24 20:15:35 +0800

    Update: 8/24/2025, 8:15:34 PM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -77,8 +77,8 @@
 
         <div class="w-83 md:w-full bg-black rounded-2xl p-3 m-1 md:text-2xl">Total Rp: <span class="text-green-500">{total}</span></div>
         <div class="w-full bg-black rounded-2xl p-3 m-1 md:text-2xl">
-          <label class="select w-[40%]">
-            <span class="label bg-">Name</span>
+          <label class="select w-[50%]">
+            <span class="label bg-black">Name ></span>
             <select bind:value={namaWaiters}>
               {#each users as d}
               <option value={d.name}>{d.name}</option>
