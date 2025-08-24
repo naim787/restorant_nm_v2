26c24d871abad6b0e8303d1badc00677a63bb877
naim <naimmmmab@gmail.com>
2025-08-24 23:25:04 +0800

    Update: 8/24/2025, 11:25:03 PM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -79,7 +79,7 @@
         </div>
 
         <div class="w-70 md:w-full bg-black rounded-2xl p-3 m-1 md:text-2xl">Total Rp: <span class="text-green-500">{total}</span></div>
-        <div class="w-70 md:w-full h-auto bg-black rounded-2xl p-3 m-1 md:text-2xl flex flex-col">
+        <div class="w-75 md:w-full h-auto bg-black rounded-2xl p-3 m-1 md:text-2xl flex flex-col">
           <div class="flex">
             <!-- // nama pemesan -->
               <label class="select w-[50%]">
