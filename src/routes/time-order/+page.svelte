acdb534ecc8270d47e48bb3b49f45d788d9caaab
naim787 <naimmmmab@gmail.com>
2025-09-01 07:53:49 -0400

    Update: 9/1/2025, 7:53:49 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -10,7 +10,7 @@
             <!-- <UserCircleSolid class="h-4 w-4 text-gray-500 dark:text-gray-400" /> -->
              hallo
             </InputAddon>
-            <Input id="website-admin" placeholder="johndoe" class="w-"/>
+            <Input id="website-admin" placeholder="johndoe" class="w-[]"/>
         </ButtonGroup>
     </div>
     <div class="w-[60%]">
