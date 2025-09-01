dc9dcce61e081e307c9c9be360c8973921c13b64
naim787 <naimmmmab@gmail.com>
2025-09-01 07:44:52 -0400

    Update: 9/1/2025, 7:44:52 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -9,7 +9,7 @@
           <TableHead class="bg-black">
             <TableHeadCell>Product name</TableHeadCell>
             <TableHeadCell>Color</TableHeadCell>
-            <TableHeadCell></TableHeadCell>
+            <TableHeadCell>Time</TableHeadCell>
             <TableHeadCell>Action</TableHeadCell>
             <TableHeadCell>
               <span class="sr-only">Edit</span>
