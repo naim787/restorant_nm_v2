89bb91d0cd6839cc5d6ea4f6a0ad873bd3254aa2
naim787 <naimmmmab@gmail.com>
2025-09-01 07:45:09 -0400

    Update: 9/1/2025, 7:45:09 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -7,7 +7,7 @@
     <div class="w-[60%]">
         <Table hoverable={true}>
           <TableHead class="bg-black">
-            <TableHeadCell>Product name</TableHeadCell>
+            <TableHeadCell></TableHeadCell>
             <TableHeadCell>No</TableHeadCell>
             <TableHeadCell>Time</TableHeadCell>
             <TableHeadCell>Action</TableHeadCell>
