5bd567a3e0abf757e2ec220741edbdc05da088c0
naim787 <naimmmmab@gmail.com>
2025-09-01 07:43:21 -0400

    Update: 9/1/2025, 7:43:21 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <div class="w-[100vw] flex justify-center">
+    <div class=""></div>
     <Table hoverable={true}>
       <TableHead class="bg-black">
         <TableHeadCell>Product name</TableHeadCell>
