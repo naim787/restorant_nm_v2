dc6f463cb94e119947e561bfc73ed99b1cff691e
naim787 <naimmmmab@gmail.com>
2025-09-01 07:58:00 -0400

    Update: 9/1/2025, 7:58:00 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -27,7 +27,7 @@
             </TableHeadCell>
           </TableHead>
           <TableBody>
-            <TableBodyRow >
+            <TableBodyRow class="">
               <TableBodyCell>Apple MacBook Pro 17</TableBodyCell>
               <TableBodyCell>Rahel</TableBodyCell>
               <TableBodyCell>Lorem ipsum dolor sit amet </TableBodyCell>
