b2965638d53ee1124bff9593b005843fc31ebcca
naim787 <naimmmmab@gmail.com>
2025-09-01 07:53:17 -0400

    Update: 9/1/2025, 7:53:17 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -3,7 +3,7 @@
   import "../../app.css"
 </script>
 
-<div class="w-[100vw] flex flex-col items-center ju">
+<div class="w-[100vw] flex flex-col items-center justify-start">
     <div class="">
         <ButtonGroup class="w-full">
             <InputAddon>
