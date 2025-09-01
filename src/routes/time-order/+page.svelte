fe13f34c0ccc3b183f7027acbf33649f578ea5f9
naim787 <naimmmmab@gmail.com>
2025-09-01 07:51:35 -0400

    Update: 9/1/2025, 7:51:35 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -6,11 +6,11 @@
 <div class="w-[100vw] flex justify-center">
     <div class="">
         <ButtonGroup class="w-full">
-    <InputAddon>
-      <UserCircleSolid class="h-4 w-4 text-gray-500 dark:text-gray-400" />
-    </InputAddon>
-    <Input id="website-admin" placeholder="johndoe" />
-  </ButtonGroup>
+            <InputAddon>
+            <UserCircleSolid class="h-4 w-4 text-gray-500 dark:text-gray-400" />
+            </InputAddon>
+            <Input id="website-admin" placeholder="johndoe" />
+        </ButtonGroup>
     </div>
     <div class="w-[60%]">
         <Table hoverable={true}>
