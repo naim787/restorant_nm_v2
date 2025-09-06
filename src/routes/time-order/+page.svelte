ef435a0d35229134cfa6d6a418ae69a3b816e8a4
naim787 <naimmmmab@gmail.com>
2025-09-06 06:11:23 -0400

    Update: 9/6/2025, 6:11:23 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -38,7 +38,7 @@
               <TableBodyCell>00:00</TableBodyCell>
               <!-- <TableBodyCell>$2999</TableBodyCell> -->
               <TableBodyCell>
-                <Button onclick={() => (defaultModal = true)} class="text-primary-600 dark:text-primary-500 font-medium hover:underline p-2 rounded-full bg-red-500/10 border border-red-500"></Button>
+                <Button onclick={() => (defaultModal = true)} class="text-primary-600 dark:text-primary-500 font-medium hover:underline p-2 rounded-full bg-red-500/10 border border-red-500">LIHAT</Button>
 
               </TableBodyCell>
             </TableBodyRow>
