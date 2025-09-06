23f422af0ecee85b54850f44d5a60e7cfc53aa5c
naim787 <naimmmmab@gmail.com>
2025-09-06 06:27:44 -0400

    Update: 9/6/2025, 6:27:44 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell,Input, InputAddon, ButtonGroup, Button, Modal, P } from "flowbite-svelte";
   import "../../app.css";
+  import { onMount } from "svelte";
 
 
   // ✅ Perbaiki format base URL
@@ -9,7 +10,7 @@
 
   let defaultModal = $state(false);
 
-  onM
+  onMount
 </script>
 
 <div class="w-[100vw] h-[100vh] flex flex-col items-center justify-start p-4 bg-black">
