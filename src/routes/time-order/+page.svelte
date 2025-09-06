e82a3c26dc3c79d26c28664e881cae17c37d94a8
naim787 <naimmmmab@gmail.com>
2025-09-06 06:29:49 -0400

    Update: 9/6/2025, 6:29:49 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -53,7 +53,9 @@
           </TableHead>
           <TableBody>
             <TableBodyRow class="bg-gray-600 text-white hover:bg-tranparent">
-              {#}
+              {#each  as }
+                
+              {/each}
               <TableBodyCell>Rahel</TableBodyCell>
               <TableBodyCell>
                 <Button onclick={() => (defaultModal = true)} class="text-yellow-600 dark:text-yellow-500 font-medium p-2 rounded-full bg-yellow-500/10 border border-yellow-500 hover:bg-yellow-500/20">LIHAT</Button>
