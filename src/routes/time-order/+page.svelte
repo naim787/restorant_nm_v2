49dee98860590de9a2200c26ac275fe4b82b8c6e
naim787 <naimmmmab@gmail.com>
2025-09-07 01:05:35 -0400

    Update: 9/7/2025, 1:05:35 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -21,7 +21,7 @@
       if (!res.ok) throw new Error(`HTTP error! status: ${res.status}`);
 
       let result = await res.json();
-      data = [...data, ]
+      data = [...data, result.data;]
     } catch (error) {
       console.error("❌ Error fetching menu:", error);
     }
