d2fd18621c9260c9cee54077c27ed1fde3dd2d13
naim787 <naimmmmab@gmail.com>
2025-09-07 02:29:48 -0400

    Update: 9/7/2025, 2:29:48 AM

--- src/routes/time-order/+page.svelte
+++ src/routes/time-order/+page.svelte
@@ -70,7 +70,7 @@
     </div>
 </div>
 
-<Modal form bind:open={defaultModal} onaction={({ action }) => alert(`berhasil men ${action}`)} class="bg-black text-white">
+<Modal form bind:open={defaultModal} onaction={({ action }) => con(`berhasil men ${action}`)} class="bg-black text-white">
 
   <h1 class="text-2xl mb-[2vh]">Lorem ipsum dolor sit amet.</h1>
   
