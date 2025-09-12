991b5c5fddca7a53ad76c9259b160612f00e3a72
naim787 <naimmmmab@gmail.com>
2025-09-12 01:58:52 -0400

    Update: 9/12/2025, 1:58:52 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -31,7 +31,6 @@
   <div class="px-5 pb-5">
     <a href="/">
     <h5 class="text-3xl font-bold font-semibold tracking-tight dark:text-white">{item.name}
-      
     </a>
     <Rating rating={5} size={24} class="mb-5">
       {#snippet text()}
