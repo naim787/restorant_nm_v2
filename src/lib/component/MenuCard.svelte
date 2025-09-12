2eb8c6eaf03c897336169f4c49882a5279357f2f
naim787 <naimmmmab@gmail.com>
2025-09-12 02:04:40 -0400

    Update: 9/12/2025, 2:04:40 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -37,7 +37,7 @@
     </Rating>
     <div class="flex items-center justify-between bg-green-500 p-2">
       <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-      <button class="btn bg-green-500 text-black text-[50px]" on:click={onClick}>pesan</button>
+      <button class="btn bg-green-500 text-black text-[20px]" on:click={onClick}>pesan</button>
     </div>
   </div>
 </Card>
