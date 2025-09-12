f11c6c9606e9d065b15ce472b5d979d71fcd88b9
naim787 <naimmmmab@gmail.com>
2025-09-12 01:43:33 -0400

    Update: 9/12/2025, 1:43:33 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -31,7 +31,8 @@
     <div class="m-6">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
       <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      <Button class="w-40"><ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+      <Button class="w-40">
+        Read more <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
       </Button>
     </div>
   </Card>
