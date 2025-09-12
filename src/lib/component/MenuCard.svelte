47bd361c6eb99a227bb28f844e9b5e538bd175c3
naim787 <naimmmmab@gmail.com>
2025-09-12 07:43:22 -0400

    Update: 9/12/2025, 7:43:22 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -3,23 +3,6 @@
   export let onClick;
   const base = import.meta.env.VITE_API_BASE || '';
 </script>
-<!-- 
-<div class="card bg-base-100 image-full w-72 h-42 shadow-sm m-2">
-  <figure>
-    <img src={`${base}${item.image_url}`} alt={item.name} loading="lazy" class="w-full h-full object-cover">
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title text-2xl">{item.name}</h2>
-    <p>{item.description}</p>
-    <div class="card-actions justify-end">
-       <div class="card-actions justify-end">
-      <button class="btn bg-green-500 text-black" on:click={onClick}>
-        Rp: <span>{item.price}</span>
-      </button>
-    </div>
-    </div>
-  </div>
-</div> -->
 
 <div class="card bg-base-100 w-70 shadow-sm h-auto">
   <figure>
