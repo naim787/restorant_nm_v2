cac16970479ed68a6c1f89820b1a6a69dcf7d3ed
naim787 <naimmmmab@gmail.com>
2025-09-12 01:45:35 -0400

    Update: 9/12/2025, 1:45:35 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -24,7 +24,7 @@
   </div>
 </div>
 
-  <Card img={`http://${base}/${item.image_url}`} class="h-50">
+  <Card img={`http://${base}/${item.image_url}`} class="h-100">
     <div class="m-6">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
       <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
