d7d1858255a4b9ea040fef6379137921dc20056f
naim787 <naimmmmab@gmail.com>
2025-09-12 01:40:18 -0400

    Update: 9/12/2025, 1:40:18 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -28,7 +28,13 @@
   <div class="p-5 bg-gray-900 rounded-b-xl">
     <div class="flex items-start h-20">
       <h2 class="card-title text-3xl uppercase">{item.name}</h2>
-    </div>ask-star bg-yellow-500" aria-label="5 star" aria-current="true"></div>
+    </div>
+    <!-- <div class="rating">
+      <div class="mask mask-star bg-yellow-500" aria-label="1 star"></div>
+      <div class="mask mask-star bg-yellow-500" aria-label="2 star"></div>
+      <div class="mask mask-star bg-yellow-500" aria-label="3 star"></div>
+      <div class="mask mask-star bg-yellow-500" aria-label="4 star"></div>
+      <div class="mask mask-star bg-yellow-500" aria-label="5 star" aria-current="true"></div>
     </div> -->
     <div class="w-full flex justify-between items-center">
       <p class="text-2xl">Rp: <span class="text-yellow-400">{item.price}</span></p>
