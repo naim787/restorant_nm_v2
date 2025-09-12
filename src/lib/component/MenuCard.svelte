06a3a1a3092d9c44e0de6df754a4a48b63045261
naim787 <naimmmmab@gmail.com>
2025-09-12 01:49:05 -0400

    Update: 9/12/2025, 1:49:05 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -25,6 +25,7 @@
 </div>
 
   <Card class="p-0">
+    <div class=""></div>
     <img class="rounded-t-lg p-8" src={`http://${base}/${item.image_url}`} alt="product 1" />
   <div class="px-5 pb-5">
     <a href="/">
