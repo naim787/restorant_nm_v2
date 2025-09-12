73d26262fd95654bc5de518b44b54ae5b6c02909
naim787 <naimmmmab@gmail.com>
2025-09-12 06:03:54 -0400

    Update: 9/12/2025, 6:03:54 AM

--- src/routes/no/orders/+page.svelte
+++ src/routes/no/orders/+page.svelte
@@ -163,7 +163,7 @@
       onCheckout={sendCheckoutData}
       on:totalChange={(e) => totalFromDrawer = e.detail}
       on:formName={(e) => namaWaiters = e.detail}
-      on:formName={(e) => namaWaiters = e.detail}
+      on:formNo={(e) => namaWaiters = e.detail}
       />
   </div>
 
