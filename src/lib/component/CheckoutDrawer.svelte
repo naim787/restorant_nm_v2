a3cc4079c8c4fea2fed1776c4b512174e6784302
naim787 <naimmmmab@gmail.com>
2025-09-12 05:55:37 -0400

    Update: 9/12/2025, 5:55:37 AM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -102,7 +102,7 @@
               <!-- OPSI -->
                 <label class="select w-full">
                   <span class="label bg-black">NO</span>
-                  <select required bind:value={formName}>
+                  <select required bind:value={formNo}>
                     <option value=""></option>
                     {#each Array.from({ length: 100 }) as _, i}
                       <option value="">{String(i + 1).padStart(2, '0')}</option>
