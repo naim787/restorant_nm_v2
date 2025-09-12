69bdff823c0c15ff7feb885f93a44e6167b931f3
naim787 <naimmmmab@gmail.com>
2025-09-12 05:49:04 -0400

    Update: 9/12/2025, 5:49:04 AM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -109,7 +109,7 @@
               <h1 class="text-yellow-500">TYPE : </h1>
                <h1 class="">NO        <input required type="radio" name="radio-1" class="" value="NO" bind:group={OPSI}/></h1>
                <h1 class="">BUGKUS    <input required type="radio" name="radio-1" class="" value="BUGKUS" bind:group={OPSI}/></h1>
-               <h1 class="">RESERFASI :<input required type="radio" name="radio-1" class="" value="RESERVASI" bind:group={OPSI}/></h1>
+               <h1 class="">RESERFASI <input required type="radio" name="radio-1" class="" value="RESERVASI" bind:group={OPSI}/></h1>
              </div>
         </div>
         <div class="w-78 md:w-full bg-black rounded-2xl p-3">
