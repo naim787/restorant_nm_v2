4c4637af603e4a60509117ada7aa6c2e0ba800db
naim787 <naimmmmab@gmail.com>
2025-09-12 08:06:58 -0400

    Update: 9/12/2025, 8:06:58 AM

--- src/lib/component/CheckoutDrawer.svelte
+++ src/lib/component/CheckoutDrawer.svelte
@@ -37,7 +37,7 @@
 
   $: dispatch("formName", namaWaiters);
   $: dispatch("formNo", formNo);
-  $: dispatch("formType", opsi);
+  $: dispatch("formType", formNo);
   $: dispatch("formOpsi", opsi);
 </script>
 
@@ -105,7 +105,7 @@
                   <select required bind:value={formNo}>
                     <option value=""></option>
                     {#each Array.from({ length: 100 }) as _, i}
-                      <option value={i + 1}>{String(i + 1).padStart(2, '0')}</option>
+                      <option value={i + }>{String(i + 1).padStart(2, '0')}</option>
                     {/each}
                   </select>
                 </label>
