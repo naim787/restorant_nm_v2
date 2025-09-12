89770567975e88061f2644c83ed1d97b9ee8b5be
naim787 <naimmmmab@gmail.com>
2025-09-12 01:47:56 -0400

    Update: 9/12/2025, 1:47:56 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -24,12 +24,22 @@
   </div>
 </div>
 
-  <Card img={`http://${base}/${item.image_url}`}  size="md" class="h-50">
-    <div class="m-6">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-      <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      <Button class="w-40">
-        Read more
-      </Button>
+  <Card class="p-0">
+  <a href="/">
+    <img class="rounded-t-lg p-8" src="/images/product-1.webp" alt="product 1" />
+  </a>
+  <div class="px-5 pb-5">
+    <a href="/">
+      <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+    </a>
+    <Rating rating={4} size={24} class="mt-2.5 mb-5">
+      {#snippet text()}
+        <Badge class="ms-3">4</Badge>
+      {/snippet}
+    </Rating>
+    <div class="flex items-center justify-between">
+      <span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+      <Button href="/">Buy now</Button>
     </div>
-  </Card>
+  </div>
+</Card>
