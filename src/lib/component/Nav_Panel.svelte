397fd33788eb50f2bb4d40fa57bf836e66c7d1cf
naim787 <naimmmmab@gmail.com>
2025-09-12 01:26:21 -0400

    Update: 9/12/2025, 1:26:21 AM

--- src/lib/component/Nav_Panel.svelte
+++ src/lib/component/Nav_Panel.svelte
@@ -5,7 +5,7 @@
 
 <Navbar>
   <NavBrand href="/">
-    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">NavPanel</span>
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
   </NavBrand>
   <NavHamburger />
   <NavUl>
