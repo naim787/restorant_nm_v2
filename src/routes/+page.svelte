fb66617c0093490c2b69b69fcc21466e96ecdb6d
naim787 <naimmmmab@gmail.com>
2025-09-12 01:05:02 -0400

    Update: 9/12/2025, 1:05:02 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,7 +16,3 @@
     <NavLi href="/contact">Contact</NavLi>
   </NavUl>
 </Navbar>
-
-<P>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quos impedit quo, quis quam in distinctio deleniti facere! Ea aliquid maiores iusto obcaecati rerum quisquam repellendus dignissimos rem quo veritatis.</P>
-
-<P>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur quos impedit quo, quis quam in distinctio deleniti facere! Ea aliquid maiores iusto obcaecati rerum quisquam repellendus dignissimos rem quo veritatis.</P>
