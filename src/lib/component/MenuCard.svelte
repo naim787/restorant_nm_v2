10bbd58bc929a708496c3063ed55d4f09d6a9d67
naim787 <naimmmmab@gmail.com>
2025-09-12 01:56:11 -0400

    Update: 9/12/2025, 1:56:11 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -30,7 +30,7 @@
     </a>
   <div class="px-5 pb-5">
     <a href="/">
-      <h5 class="text-xl font-semibold tracking-tight dark:text-white">A</a>
+      <h5 class="text-xl font-semibold tracking-tight dark:text-white"></a>
     <Rating rating={4} size={24} class="mt-2.5 mb-5">
       {#snippet text()}
         <Badge class="ms-3">4</Badge>
