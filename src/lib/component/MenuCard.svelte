e3040f59180728bcfa9bda8961d1948523d74156
naim787 <naimmmmab@gmail.com>
2025-09-12 02:07:48 -0400

    Update: 9/12/2025, 2:07:48 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -7,7 +7,7 @@
   const base = import.meta.env.VITE_API_BASE || '';
 </script>
 
-<div class="card bg-base-100 w-70 shadow-sm h-auto">
+<!-- <div class="card bg-base-100 w-70 shadow-sm h-auto">
   <figure>
     <img class="w-full h-60 object-cover" src={`http://${base}/${item.image_url}`} alt={item.name} loading="lazy" />
   </figure>
@@ -22,7 +22,7 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
   <Card class="p-0 bg-gray-900 text-white">
     <a href="/" class="w-full h-50">
