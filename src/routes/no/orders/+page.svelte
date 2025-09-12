ef2718deba50dc179a84a86f0a79149e164a51b9
naim787 <naimmmmab@gmail.com>
2025-09-12 05:31:25 -0400

    Update: 9/12/2025, 5:31:25 AM

--- src/routes/no/orders/+page.svelte
+++ src/routes/no/orders/+page.svelte
@@ -96,7 +96,7 @@
 
     if (socket && socket.readyState === WebSocket.OPEN && checkoutData.length > 0 ) {
       if ( namaWaiters == "") {
-        alert()
+        alert("")
       }
       // Masukkan total ke setiap objek ordersArray
       const orderArray = checkoutData.map(item => ({
