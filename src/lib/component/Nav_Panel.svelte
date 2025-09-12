439335825d32de1ddcafa6fe4f1763719dd162fe
naim787 <naimmmmab@gmail.com>
2025-09-12 01:34:03 -0400

    Update: 9/12/2025, 1:34:03 AM

--- src/lib/component/Nav_Panel.svelte
+++ src/lib/component/Nav_Panel.svelte
@@ -1,7 +1,7 @@
 <script>
   import "../../app.css";
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
-  import ConfigurateData from "$lib/component/conf.js"
+  import ConfigurateData from "$lib/component/conf.js";
 </script>
 
 <Navbar>
