e4de0e31c691955309fa522c70ca8c678b64b3f5
naim787 <naimmmmab@gmail.com>
2025-09-12 01:56:09 -0400

    Update: 9/12/2025, 1:56:09 AM

--- src/lib/component/MenuCard.svelte
+++ src/lib/component/MenuCard.svelte
@@ -30,8 +30,7 @@
     </a>
   <div class="px-5 pb-5">
     <a href="/">
-      <h5 class="text-xl font-semibold tracking-tight dark:text-white">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
-    </a>
+      <h5 class="text-xl font-semibold tracking-tight dark:text-white">A</a>
     <Rating rating={4} size={24} class="mt-2.5 mb-5">
       {#snippet text()}
         <Badge class="ms-3">4</Badge>
