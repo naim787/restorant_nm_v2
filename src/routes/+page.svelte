cd1f96df44029b4000f4bb090372cf800454a0fa
naim787 <naimmmmab@gmail.com>
2025-09-12 01:05:31 -0400

    Update: 9/12/2025, 1:05:31 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@
 
 <Navbar>
   <NavBrand href="/">
-    <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
   </NavBrand>
   <NavHamburger />
