ee56db03bce7759aa654fa0f58c89111deea2256
naim787 <naimmmmab@gmail.com>
2025-09-12 08:04:58 -0400

    Update: 9/12/2025, 8:04:58 AM

--- src/routes/no/orders/+page.svelte
+++ src/routes/no/orders/+page.svelte
@@ -98,7 +98,7 @@
     if (socket && socket.readyState === WebSocket.OPEN && checkoutData.length > 0 ) {
       if ( namaWaiters == "") {
         alert("❌ mohon masukan dulu nama waiters!!")
-      } else if(formNo == "",) {
+      } else if(formNo == "" && ) {
 
       }
       else {
