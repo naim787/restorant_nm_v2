c6c24258578af942f2d4fc373a9dfc15ccbd2f45
naim787 <naimmmmab@gmail.com>
2025-10-18 04:01:03 -0400

    Update: 10/18/2025, 4:01:03 AM

--- src/routes/dashbord/admin/+page.svelte
+++ src/routes/dashbord/admin/+page.svelte
@@ -1,20 +1,27 @@
 <script>
+  import { page } from "$app/state";
   import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
-  import { fly } from "svelte/transition";
-  import { OpenSidebarAltSolid } from "flowbite-svelte-icons";
+  import { LayersSolid } from "flowbite-svelte-icons";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  let activeUrl = $derived(page.url.pathname);
 </script>
 
-<Navbar>
+<Navbar class="border-b-1 dark:border-white/20 shadow-2xl shadow-white/15 dark:bg-gray-800 ">
   <NavBrand href="/">
-    <h1 class=""><OpenSidebarAltSolid class="shrink-0 h-6 w-6" /></h1>
-    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+    <img src="./favicon.webp" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">SMANDIM</span>
   </NavBrand>
-  <NavHamburger />
-  <NavUl transition={fly} transitionParams={{ y: -20, duration: 250 }}>
+  <button class="p-2 bg-gray-400 rounded-md md:hidden" on:click={() => {dispatch('panelDash');}}>
+    <LayersSolid class="shrink-0 h-6 w-6" />
+  </button>
+  <NavUl {activeUrl}>
     <NavLi href="/">Home</NavLi>
-    <NavLi href="/about">About</NavLi>
     <NavLi href="/docs/components/navbar">Navbar</NavLi>
-    <NavLi href="/pricing">Pricing</NavLi>
-    <NavLi href="/contact">Contact</NavLi>
+    <NavLi href="/docs/components/accordion">Accordion</NavLi>
+    <NavLi href="/docs/components/alert">Alert</NavLi>
+    <NavLi href="/docs/components/avatar">Avatar</NavLi>
   </NavUl>
+  
 </Navbar>
