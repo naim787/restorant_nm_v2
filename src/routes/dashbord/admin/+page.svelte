0dd0ea4174b7db4006fab322069198c75cd18050
naim787 <naimmmmab@gmail.com>
2025-10-18 04:08:43 -0400

    Update: 10/18/2025, 4:08:43 AM

--- src/routes/dashbord/admin/+page.svelte
+++ src/routes/dashbord/admin/+page.svelte
@@ -2,7 +2,6 @@
 	import NavvDash from './../../lib/components/NavvDash.svelte';
   import Footer from "$lib/components/Footer.svelte";
   import Nav from "$lib/components/Nav.svelte";
-  import "../../app.css";
   import PaannelDash from '$lib/components/PaannelDash.svelte';
 
   import { AccordionItem, Accordion } from "flowbite-svelte";
@@ -15,21 +14,20 @@
 </script>
 
 <!-- <Nav /> -->
- <NavvDash on:panelDash={() => {Bars = !Bars}} />
-    <div class="w-full h-[90vh] flex bg-gray-200">
-        <!-- navbar -->
-       <PaannelDash Bars={Bars} />
-        <!-- <h1 class="text-3xl tcext-red-500 m-auto">{message}</h1> -->
-         <div class="w-full bg-gray-200 dark:bg-gray-900 flex flex-col justify-start items-start p-2"
-          role="button"
-          tabindex="0"
-          on:click={() => Bars = true}
-          on:keypress={(e) => { if (e.key === 'Enter') Bars = true; }}
-         >
-            <div class="w-full p-3"><h1 class="text-4xl font-bold gloria ">Dashbord</h1></div>
-            <div class="p-3 bg-white rounded-2xl">
-                <!-- <canvas bind:this={chartCanvas} id="myChart"></canvas> -->
-            </div>
-         </div>
-    </div>
-<Footer />
+<NavvDash on:panelDash={() => {Bars = !Bars}} />
+<div class="w-full h-[90vh] flex bg-gray-200">
+    <!-- navbar -->
+    <PaannelDash Bars={Bars} />
+    <!-- <h1 class="text-3xl tcext-red-500 m-auto">{message}</h1> -->
+      <div class="w-full bg-gray-200 dark:bg-gray-900 flex flex-col justify-start items-start p-2"
+      role="button"
+      tabindex="0"
+      on:click={() => Bars = true}
+      on:keypress={(e) => { if (e.key === 'Enter') Bars = true; }}
+      >
+        <div class="w-full p-3"><h1 class="text-4xl font-bold gloria ">Dashbord</h1></div>
+        <div class="p-3 bg-white rounded-2xl">
+            <!-- <canvas bind:this={chartCanvas} id="myChart"></canvas> -->
+        </div>
+      </div>
+</div>
