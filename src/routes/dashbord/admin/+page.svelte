817a00767476d3113ea336d9105eab5078d0e287
naim787 <naimmmmab@gmail.com>
2025-10-18 22:02:20 -0400

    Update: 10/18/2025, 10:02:20 PM

--- src/routes/dashbord/admin/+page.svelte
+++ src/routes/dashbord/admin/+page.svelte
@@ -2,4 +2,8 @@
 
 </script>
 
-<div class="w-[200px] h-[100vh] bg-black"></div>
+<div class="w-full h-auto flex flex-row justify-center items-center">
+    <div class="w-[280px] h-[100vh] bg-gray-800">
+    </div>
+    <div class="w-full h-[100vh] bg-red-500"></div>
+</div>
