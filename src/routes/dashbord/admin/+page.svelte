aeb10dfcdbbdd1cb537d50ee5cc36fc8e899b446
naim787 <naimmmmab@gmail.com>
2025-10-18 22:04:50 -0400

    Update: 10/18/2025, 10:04:50 PM

--- src/routes/dashbord/admin/+page.svelte
+++ src/routes/dashbord/admin/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="w-full h-auto flex flex-row justify-center items-center">
-    <div class="w-[280px] h-[100vh] bg-gray-800">
+    <div class="w-[350px] h-[100vh] bg-gray-400">
     </div>
-    <div class="w-full h-[100vh] bg-red-500"></div>
+    <div class="w-full h-[100vh]"></div>
 </div>
