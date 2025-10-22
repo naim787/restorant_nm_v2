71f0b6da38df8037524fcad86df093fc09502184
naim787 <naimmmmab@gmail.com>
2025-10-22 11:46:51 -0400

    Update: 10/22/2025, 11:46:51 AM

--- src/api/aut/+server.js
+++ src/api/aut/+server.js
@@ -1,9 +1,9 @@
 import { json } from '@sveltejs/kit'
 export async function POST({request, cookies}) {
-    const {description} = await request.json()
+    const {body} = await request.json()
 
 
-    console.log(description)
+    console.log(body.password)
     return json({
 		// get a specific field's value
 		name: body.get('name') ?? 'world'
