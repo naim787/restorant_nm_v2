48cc654db40d0b5b9a1d47fac868f9acbe18d4a6
naim787 <naimmmmab@gmail.com>
2025-10-25 11:48:49 -0400

    Update: 10/25/2025, 11:48:49 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,9 @@
 <script>
-  import { Card, Button } from "flowbite-svelte";
-  import { ArrowRightOutline } from "flowbite-svelte-icons";
-  let vCard = false;
+  import MenuCard from "$lib/components/user/MenuCard.svelte";
 </script>
 
-<div class="space-y-4">
-  <Card img="https://assets.telkomsel.com/public/2024-12/makanan-khas-indonesia.png?VersionId=TfbS8hN3yl4fLIANXfbipwkFx1Fq.KKU" reverse={vCard}>
-    <div class="m-6">
-      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-      <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      <Button class="w-40">
-        Read more <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
-      </Button>
-    </div>
-  </Card>
-  <!-- <Toggle bind:checked={vCard} class="italic dark:text-gray-500">Reverse</Toggle> -->
+<div class="w-[100vw] h-[100vh] flex flex-wrap justify-center items-center">
+  <MenuCard />
+  <MenuCard />
+  <MenuCard />
 </div>
