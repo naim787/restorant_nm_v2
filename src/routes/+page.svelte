cbf8f0d1fcb9c8870f46ab760c32a0b9c877470a
naim787 <naimmmmab@gmail.com>
2025-10-25 11:44:29 -0400

    Update: 10/25/2025, 11:44:29 AM

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="space-y-4">
-  <Card img="/images/image-1.webp" reverse={vCard}>
+  <Card img="https://assets.telkomsel.com/public/2024-12/makanan-khas-indonesia.png?VersionId=TfbS8hN3yl4fLIANXfbipwkFx1Fq.KKU" reverse={vCard}>
     <div class="m-6">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
       <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
