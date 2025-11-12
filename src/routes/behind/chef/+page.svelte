636296e101c92463b3cb0d8d9ed652f639c82481
naim787 <naimmmmab@gmail.com>
2025-11-12 03:28:38 -0500

    Update: 11/12/2025, 3:28:38 AM

--- src/routes/behind/chef/+page.svelte
+++ src/routes/behind/chef/+page.svelte
@@ -1,5 +1,13 @@
 <script>
-
+  import Footer from "$lib/components/user/Footer.svelte";
+  import MenuCard from "$lib/components/user/MenuCard.svelte";
+  import MenuNav from "$lib/components/user/MenuNav.svelte";
 </script>
 
-<div class=""></div>
+<MenuNav />
+<div class="w-[100vw] h-[100vh] overflow-scroll flex flex-wrap justify-evenly items-center pt-15">
+  <MenuCard />
+  <MenuCard />
+  <MenuCard />
+</div>
+<Footer/>
