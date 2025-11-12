07974714d5ab4519c87cbb76db89de3a68b1c13a
naim787 <naimmmmab@gmail.com>
2025-11-12 18:23:29 -0500

    Update: 11/12/2025, 6:23:29 PM

--- src/routes/behind/chef/+page.svelte
+++ src/routes/behind/chef/+page.svelte
@@ -5,31 +5,38 @@
   import { ArrowRightOutline } from "flowbite-svelte-icons";
 
   let data = [
-    "tinutuan.webp",
-    "soto ayam.webp",
+   "tinutuan.webp",
+    "soto_ayam.webp",
     "nasi_ayam_goreg_mentega.webp",
     "rawon.webp",
-    "nasi goreg kawan baru.webp",
-    "nasi goreg kambig.webp"
+    "nasi_goreg_kawan_baru.webp",
+    "nasi_goreg_kambig.webp",
+    "nasi_ayam_isi_bulu.webp",
+    "nasi_ayam_rempah.webp",
+    "nasi_campur_nusantara.webp",
+    "nasi_capcay.webp",
+    "nasi_goreg_ayam.webp",
+    "nasi_goreg_cakalag.webp"
   ]
 </script>
 
 <MenuNav />
 <div class="w-[100vw] h-[100vh] overflow-scroll flex flex-wrap justify-evenly items-center pt-15">
     <div class="space-y-4 m-2">
-
-        <Card img="/soto ayam.webp">
-            <!-- <img src="/nasi_ayam_goreg_mentega.webp" alt="" class="w-full object-cover"> -->
-            <div class="m-6">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">porsi : 12</p>
-            <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">tendo : 15</p>
-            <Button class="w-full bg-amber-600">
-                Read more <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
-            </Button>
-            </div>
-        </Card>
+        {#each data as d}
+            <Card img={`/${d}`}>
+                <!-- <img src="/nasi_ayam_goreg_mentega.webp" alt="" class="w-full object-cover"> -->
+                <div class="m-6">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+                <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">porsi : 12</p>
+                <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">tendo : 15</p>
+                <Button class="w-full bg-amber-600">
+                    Read more <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+                </Button>
+                </div>
+            </Card>
+        {/each}
 
     </div>
 </div>
-<Footer/>/'ukkkk/9lj'
+<Footer/>/
