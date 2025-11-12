8c7018dd00bf9ade7c7f362440a148e354babbae
naim787 <naimmmmab@gmail.com>
2025-11-12 07:30:59 -0500

    Update: 11/12/2025, 7:30:59 AM

--- src/routes/behind/chef/+page.svelte
+++ src/routes/behind/chef/+page.svelte
@@ -1,13 +1,23 @@
 <script>
   import Footer from "$lib/components/user/Footer.svelte";
-  import MenuCard from "$lib/components/user/MenuCard.svelte";
   import MenuNav from "$lib/components/user/MenuNav.svelte";
+  import { Card, Button } from "flowbite-svelte";
+  import { ArrowRightOutline } from "flowbite-svelte-icons";
 </script>
 
 <MenuNav />
 <div class="w-[100vw] h-[100vh] overflow-scroll flex flex-wrap justify-evenly items-center pt-15">
-  <MenuCard />
-  <MenuCard />
-  <MenuCard />
+    <div class="space-y-4 m-2">
+        <Card img="./nasi_ayam_goreg_mentega.webp">
+            <div class="m-6">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">porsi : 12</p>
+            <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">tendo : 15</p>
+            <Button class="w-full bg-amber-600">
+                Read more <ArrowRightOutline class="ms-2 h-6 w-6 text-white" />
+            </Button>
+            </div>
+        </Card>
+    </div>
 </div>
 <Footer/>
