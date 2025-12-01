740e4e8dbb01e5b1dda63089fa9b76de4fcb601a
naim787 <naimmmmab@gmail.com>
2025-12-01 07:48:58 -0500

    Update: 12/1/2025, 7:48:58 AM

--- src/lib/components/user/MenuCard.svelte
+++ src/lib/components/user/MenuCard.svelte
@@ -1,9 +1,25 @@
 <script>
   import { Card, Button } from "flowbite-svelte";
   import { ArrowRightOutline } from "flowbite-svelte-icons";
+  import Modal from "./Modal.svelte";
+
+  import {Modal, P } from "flowbite-svelte";
+
+  let defaultModal = $state(false);
 </script>
 
+<Modal title="Terms of Service" form bind:open={defaultModal} onaction={({ action }) => alert(`Handle "${action}"`)}>
+  <P>With less than a month to go before the European Union enacts new consumer privacy laws for its citizens, companies around the world are updating their terms of service agreements to comply.</P>
+  <P>
+    The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant to ensure a common set of data rights in the European Union. It requires organizations to
+    notify users as soon as possible of high-risk data breaches that could personally affect them.
+  </P>
 
+  {#snippet footer()}
+    <Button type="submit" value="success">I accept</Button>
+    <Button type="submit" value="decline" color="alternative">Decline</Button>
+  {/snippet}
+</Modal>
 
 
 <div class="space-y-4 m-2">
